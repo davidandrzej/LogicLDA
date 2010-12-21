9cb0ca1e730cb42a0e0e4acf2f07b570085d3004
David Andrzejewski <andrzejewski1@llnl.gov>
2010-12-21 14:34:59 -0800

    bash script to fetch OpenNLP models

---
+++ getmodels.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir models
+cd models
+wget http://opennlp.sourceforge.net/models/english/tokenize/EnglishTok.bin.gz
+wget http://opennlp.sourceforge.net/models/english/sentdetect/EnglishSD.bin.gz
