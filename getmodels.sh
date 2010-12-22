4dd4ddd1581611195f7091a25ee0f6466945047b
David Andrzejewski <andrzejewski1@llnl.gov>
2010-12-21 16:23:03 -0800

    tweak eval script

--- getmodels.sh
+++ getmodels.sh
@@ -4,3 +4,4 @@ mkdir models
 cd models
 wget http://opennlp.sourceforge.net/models/english/tokenize/EnglishTok.bin.gz
 wget http://opennlp.sourceforge.net/models/english/sentdetect/EnglishSD.bin.gz
+wget http://jmlr.csail.mit.edu/papers/volume5/lewis04a/a11-smart-stop-list/english.stop
