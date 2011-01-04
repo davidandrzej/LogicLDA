4ff1e5a60271bdf4663ac27c43dd9dbbff936bfb
David Andrzejewski <david.andrzej@gmail.com>
2011-01-04 10:44:41 -0800

    runall tweak

--- runAll.sh
+++ runAll.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for SCHEME in "LDA" "CGS" "MWS" "MIR" "MPL" "ALC"
+# for SCHEME in "LDA" "CGS" "MWS" "MIR" "MPL" "ALC"
+for SCHEME in "LDA" "MIR"
 do
 bash runEval.sh $SCHEME $1
 done
