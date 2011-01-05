3646dc0be4047aad5e2409c4edfd52f81b4233a2
David Andrzejewski <david.andrzej@gmail.com>
2011-01-05 09:03:33 -0800

    bash script tweaks

---
+++ countGround.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# java -jar ./target/logiclda-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/projects/logic-grid/s1MLAB/s1MLAB 5 
+# java -jar ./target/logiclda-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/projects/logic-grid/s2CLAB/s2CLAB 5 
+# java -jar ./target/logiclda-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/projects/logic-grid/macvspc/macvspc 5
+# java -jar ./target/logiclda-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/projects/logic-grid/20news-allcomp/20news-allcomp 5
+# java -jar ./target/logiclda-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/projects/logic-grid/congress/congress 5
+java -jar ./target/logiclda-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/projects/logic-grid/polarity/polarity 1
+# java -jar ./target/logiclda-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/projects/logic-grid/stewart/stewart 5
+#java -jar ./target/logiclda-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/projects/logic-grid/stewart-sentexcl/stewart-sentexclp 5
--- runAll.sh
+++ runAll.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# for SCHEME in "LDA" "CGS" "MWS" "MIR" "MPL" "ALC"
-for SCHEME in "LDA" "MIR"
+# "ALC"
+
+# "s1MLAB" "s2CLAB" "macvspc"
+for DATASET in "20news-allcomp" "congress"
 do
-echo "running $SCHEME $1"
-time bash runEval.sh $SCHEME $1 > $SCHEME-$1.runtime
+    for SCHEME in "LDA" "CGS" "MWS" "MIR" "MPL"
+    do
+        echo "running $SCHEME $DATASET"
+        time bash runEval.sh $SCHEME $DATASET > $SCHEME-$DATASET.runtime
+    done
+done
+
+for DATASET in "polarity" "stewart"
+do
+    for SCHEME in "LDA" "MIR"
+    do
+        echo "running $SCHEME $DATASET"
+        time bash runEval.sh $SCHEME $DATASET > $SCHEME-$DATASET.runtime
+    done
 done
