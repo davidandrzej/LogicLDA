259352d16bcccb456b03c2dd521b534e26bd225b
David Andrzejewski <david.andrzej@gmail.com>
2011-05-16 16:23:50 -0700

    remove eval bash scripts

--- getmodels.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir models
-cd models
-wget http://opennlp.sourceforge.net/models/english/tokenize/EnglishTok.bin.gz
-wget http://opennlp.sourceforge.net/models/english/sentdetect/EnglishSD.bin.gz
-wget http://jmlr.csail.mit.edu/papers/volume5/lewis04a/a11-smart-stop-list/english.stop
